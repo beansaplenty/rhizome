c1d8d6d483d79b7782c51d7920d3973dcdc73707
Nick Manning <nickmanning@nicks-air.mynetworksettings.com>
2025-11-12 11:00:30 -0500

    Fix Flask port for Render

--- app.py
+++ app.py
@@ -58,4 +58,7 @@ def edit(title):
     return render_template("edit.html", title=title, content=content)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
